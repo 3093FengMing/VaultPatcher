b85880a3d568766bbe24adab2b918dc10497a10b
3093FengMing <83573615+3093FengMing@users.noreply.github.com>
2023-01-21 10:58:44 +0800

    1.2.6 cleanup code

--- src/main/java/me/fengming/vaultpatcher/config/VaultPatcherPatch.java
+++ src/main/java/me/fengming/vaultpatcher/config/VaultPatcherPatch.java
@@ -101,9 +101,8 @@ public class VaultPatcherPatch {
 
     private boolean matchStack(String str, StackTraceElement[] stack) {
         String s = str.toLowerCase();
-        List<StackTraceElement> stackTrace = Arrays.asList(stack);
-        stackTrace = stackTrace.subList(7, stackTrace.size() - 13);
-        for (StackTraceElement ste : stackTrace) {
+        stack = Arrays.copyOfRange(stack, 7, 13);
+        for (StackTraceElement ste : stack) {
             if (s.startsWith("#")) {
                 return ste.getClassName().endsWith(s);
             } else if (s.startsWith("@")) {
