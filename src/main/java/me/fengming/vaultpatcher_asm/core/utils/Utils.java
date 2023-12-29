b36eabe88e943cf96d161e0b2c1f6810821a9161
FengMing <83573615+3093FengMing@users.noreply.github.com>
2023-12-29 22:39:03 +0800

    fix4

--- src/main/java/me/fengming/vaultpatcher_asm/core/utils/Utils.java
+++ src/main/java/me/fengming/vaultpatcher_asm/core/utils/Utils.java
@@ -56,7 +56,7 @@ public class Utils {
     // transformer
 
     public static byte[] nodeToBytes(ClassNode node) {
-        ClassWriter wr = new ClassWriter(0);
+        ClassWriter wr = new ClassWriter(ClassWriter.COMPUTE_FRAMES);
         node.accept(wr);
         return wr.toByteArray();
     }
--- src/main/java/me/fengming/vaultpatcher_asm/loader/forge/LinkClassTransformer.java
+++ src/main/java/me/fengming/vaultpatcher_asm/loader/forge/LinkClassTransformer.java
@@ -1,5 +1,6 @@
 package me.fengming.vaultpatcher_asm.loader.forge;
 
+import me.fengming.vaultpatcher_asm.config.TranslationInfo;
 import me.fengming.vaultpatcher_asm.core.transformers.VPClassTransformer;
 import me.fengming.vaultpatcher_asm.core.utils.Utils;
 import org.objectweb.asm.tree.ClassNode;
@@ -9,10 +10,14 @@ import org.objectweb.asm.ClassReader;
 public class LinkClassTransformer implements IClassTransformer {
     @Override
     public byte[] transform(String name, String transformedName, byte[] basicClass) {
+        TranslationInfo info = VPLaunchTweaker.classFinding.getOrDefault(name, null);
+        if (info == null) return basicClass;
+
         ClassNode o = new ClassNode();
         ClassReader cr = new ClassReader(basicClass);
-        cr.accept(o, ClassReader.SKIP_FRAMES | ClassReader.SKIP_DEBUG);
-        new VPClassTransformer(VPLaunchTweaker.classFinding.get(name)).accept(o);
+        cr.accept(o, 0);
+
+        new VPClassTransformer(VPLaunchTweaker.classFinding.getOrDefault(name, null)).accept(o);
         return Utils.nodeToBytes(o);
     }
 }
