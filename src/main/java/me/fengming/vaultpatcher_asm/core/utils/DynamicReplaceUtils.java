3c5885f3a4cb051616cf5ab58907fc1367fc0d67
3093FengMing <83573615+3093FengMing@users.noreply.github.com>
2025-05-23 00:40:20 +0800

    refactor 1.4.4+4

--- src/main/java/me/fengming/vaultpatcher_asm/core/utils/DynamicReplaceUtils.java
+++ src/main/java/me/fengming/vaultpatcher_asm/core/utils/DynamicReplaceUtils.java
@@ -3,45 +3,35 @@ package me.fengming.vaultpatcher_asm.core.utils;
 import me.fengming.vaultpatcher_asm.config.TargetClassInfo;
 import me.fengming.vaultpatcher_asm.config.TranslationInfo;
 
+import java.util.Arrays;
+
 @SuppressWarnings("unused")
 public class DynamicReplaceUtils {
 
     // for dynamic replace
-    public static String __mappingString(String orignal, String method) {
-        if (orignal == null) return null;
-        if (StringUtils.isBlank(orignal)) return orignal;
+    public static String __mappingString(String original, String method) {
+        if (original == null) return null;
+        if (StringUtils.isBlank(original)) return original;
         // There will be no need to get the stack traces if classname is not needed
-        StackTraceElement[] stackTraces = new StackTraceElement[0];
-        if (Utils.needStacktrace) stackTraces = Thread.currentThread().getStackTrace();
+        StackTraceElement[] stackTraces = Utils.needStacktrace ? Thread.currentThread().getStackTrace() : new StackTraceElement[0];
         for (TranslationInfo info : Utils.dynTranslationInfos) {
-            String replaced = MatchUtils.matchPairs(info.getPairs(), orignal, true);
-            if (StringUtils.isBlank(replaced) || replaced.equals(orignal)) continue;
+            String replaced = MatchUtils.matchPairs(info.getPairs(), original, true);
+            if (StringUtils.isBlank(replaced) || replaced.equals(original)) continue;
 
-            Utils.printDebugInfo(-1, orignal, method, replaced, stackTraces2String(stackTraces), info);
+            Utils.printDebugInfo(-1, original, method, replaced, stackTraces2String(stackTraces), info);
 
-            TargetClassInfo targetClass = info.getTargetClassInfo();
-            String className = targetClass.getDynamicName();
+            String className = info.getTargetClass();
             if (StringUtils.isBlank(className)) return replaced;
 
-            String methodName = targetClass.getMethod();
+            String methodName = info.getTargetClassInfo().getMethod();
             boolean ignoredMethod = StringUtils.isBlank(methodName);
 
             for (StackTraceElement stackTrace : stackTraces) {
                 if (!ignoredMethod && !methodName.equals(stackTrace.getMethodName())) continue;
-                switch (targetClass.getMatchMode()) {
-                    case FULL:
-                        if (stackTrace.getClassName().equals(className)) return replaced;
-                        break;
-                    case STARTS:
-                        if (stackTrace.getClassName().startsWith(className)) return replaced;
-                        break;
-                    case ENDS:
-                        if (stackTrace.getClassName().endsWith(className)) return replaced;
-                        break;
-                }
+                if (stackTrace.getClassName().equals(className)) return replaced;
             }
         }
-        return orignal;
+        return original;
     }
 
     private static String stackTraces2String(StackTraceElement[] stackTraces) {
