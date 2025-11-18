bc0b3031a335ce9c1456b13190bd2901817f30c3
Teddy Li <teddyxlandlee@hotmail.com>
2025-11-18 14:57:39 +0800

    Get rid of direct Unsafe invocation, in case of compile warnings

--- src/main/java/me/fengming/vaultpatcher_asm/core/misc/UnsafeVPClassLoader.java
+++ src/main/java/me/fengming/vaultpatcher_asm/core/misc/UnsafeVPClassLoader.java
@@ -1,29 +1,57 @@
 package me.fengming.vaultpatcher_asm.core.misc;
 
 import me.fengming.vaultpatcher_asm.VaultPatcher;
-import sun.misc.Unsafe;
 
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
 import java.lang.invoke.MethodType;
 import java.lang.reflect.Field;
 
-public class UnsafeVPClassLoader {
-    public static Unsafe unsafe;
-    public static MethodHandles.Lookup lookup;
+final class UnsafeVPClassLoader {
+    private static final MethodHandles.Lookup lookup;
 
     static {
         try {
             // Java 24??
-            Field Field_theUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
-            Field_theUnsafe.setAccessible(true);
-            unsafe = (Unsafe) Field_theUnsafe.get(null);
-            Field Field_IMPL_LOOKUP = MethodHandles.Lookup.class.getDeclaredField("IMPL_LOOKUP");
-            lookup = (MethodHandles.Lookup) unsafe.getObject(unsafe.staticFieldBase(Field_IMPL_LOOKUP), unsafe.staticFieldOffset(Field_IMPL_LOOKUP));
+            MethodHandles.Lookup publicLookup = MethodHandles.publicLookup();
+
+            Class<?> unsafeClass = Class.forName("sun.misc.Unsafe");
+            Field unsafeField = unsafeClass.getDeclaredField("theUnsafe");
+            unsafeField.setAccessible(true);
+            Object theUnsafe = publicLookup.unreflectGetter(unsafeField).invoke();
+
+            Field implLookupField = MethodHandles.Lookup.class.getDeclaredField("IMPL_LOOKUP");
+
+            lookup = (MethodHandles.Lookup) getField(publicLookup, unsafeClass).invoke(theUnsafe, implLookupField);
         } catch (Throwable e) {
             VaultPatcher.LOGGER.error("[VaultPatcher] Failed to initialize VPClassLoader: ", e);
             throw new RuntimeException(e);
         }
+    }
+
+    // (UF)L
+    private static MethodHandle getField(MethodHandles.Lookup lookup, Class<?> unsafeClass) throws Throwable {
+        // Unsafe.staticFieldBase(Field) -> Object
+        // Unsafe.staticFieldOffset(Field) -> long
+        // Unsafe.getObject(Object, long) -> Object
+
+        // (UOJ)L
+        MethodHandle m = lookup.findVirtual(unsafeClass, "getObject", MethodType.methodType(Object.class, Object.class, long.class));
+
+        // (UOUF)L
+        m = MethodHandles.collectArguments(
+                m, 2,
+                // (UF)J
+                lookup.findVirtual(unsafeClass, "staticFieldOffset", MethodType.methodType(long.class, Field.class))
+        );
+        // (UUFUF)L
+        m = MethodHandles.collectArguments(
+                m, 1,
+                // (UF)O
+                lookup.findVirtual(unsafeClass, "staticFieldBase", MethodType.methodType(Object.class, Field.class))
+        );
+        // (UF)L
+        return MethodHandles.permuteArguments(m, MethodType.methodType(Object.class, unsafeClass, Field.class), 0, 0, 1, 0, 1);
     }
 
     // misc
